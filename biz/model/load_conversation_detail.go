69b4321df28bb198d1751d5d2e3a8a313de81e60
asu <asueeer@163.com>
2021-11-07 18:27:46 +0800

    fix: Limit  int64

--- biz/model/load_conversation_detail.go
+++ biz/model/load_conversation_detail.go
@@ -5,7 +5,7 @@ import "ai_helper/biz/model/vo"
 type LoadConversationDetailRequest struct {
 	ConvID string `form:"conv_id" json:"conv_id" binding:"required"` // 会话id
 	Cursor string `form:"cursor" json:"cursor"`                      // 客户端本地消息的存储位置(时间戳)，用于从服务端获取该位置之后的消息
-	Limit  string `form:"limit" json:"limit"`                        // 默认50条
+	Limit  int64  `form:"limit" json:"limit"`                        // 默认50条
 	Role   string `form:"role" json:"role"`                          // 用户身份; "visitor" 游客; "be_helper": 后台客服
 }
 

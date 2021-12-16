9637102af3be2c14f6d403ae5c7c6ecaa41efe3b
asu <asueeer@163.com>
2021-12-16 16:17:20 +0800

    remove magic var

--- biz/dal/db/init.go
+++ biz/dal/db/init.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 var DB *gorm.DB
@@ -10,7 +11,8 @@ var DB *gorm.DB
 func InitDB() {
 	db, err := gorm.Open("postgres", "postgres://nearby:nearby123@47.104.186.111/nearby_boe_test?sslmode=disable")
 	if err != nil {
-		panic("数据库连接失败" + err.Error())
+		log.Println("数据库连接失败" + err.Error())
+		//panic("数据库连接失败" + err.Error())
 	}
 	DB = db
 }

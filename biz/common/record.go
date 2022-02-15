7f623832ea7a7397117725df4d23cc288452239c
asu <asueeer@163.com>
2022-02-15 21:50:35 +0800

    bugfix: 之前粗心把所有用户都误认为是客服了

--- biz/common/record.go
+++ biz/common/record.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"ai_helper/biz/domain/val_obj"
+	"github.com/spf13/cast"
 )
 
 var Record = map[string]*val_obj.UserClaims{
@@ -30,5 +31,10 @@ var Record = map[string]*val_obj.UserClaims{
 }
 
 func IsHelper(userID string) bool {
-	return Record[userID] != nil && Record[userID].IsHelper
+	for _, user := range Record {
+		if user.UserID == cast.ToInt64(userID) {
+			return true
+		}
+	}
+	return false
 }
